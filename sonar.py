39b1685c4278a5dc7837e335361d40e8a2afa08d
Matolay Pal <matolaypal@gmail.com>
2017-07-14 02:59:43 +0200

    git clone and sonar scan

--- sonar.py
+++ sonar.py
@@ -1,8 +1,10 @@
 import os
+import git
+import requests
 
 
-def run_scanner(repo_names=[]):
-    for name in repo_names:
+def run_scanner(name):
+    # for name in repo_names:
         # os.system("cd example/" + name + "/")
         with open(name + "/sonar-project.properties", "w") as text_file:
             text_file.write("sonar.projectKey=" + name + "\nsonar.sources=src\nsonar.sourceEncoding=UTF-8")
@@ -13,7 +15,40 @@ def run_scanner(repo_names=[]):
         os.chdir("../")
         # os.system("ls")
 
-run_scanner(['java-codecov-example-master', "some_scripts"])
+
+# Check the request response status, if not 200, then exit (and wite out infos)
+def check_status(resp):
+    status = resp.status_code
+    if status!=200:
+        print("URL: " + resp.url)
+        print("Status: " + str(status))
+        print("Response message: " + resp.text)
+        exit()
+
+# Get all GitHub repository from the given username,
+# then order its name and url in a dict
+git_usr = input("Enter your github username: ")
+url = "https://api.github.com/users/"+git_usr+"/repos"
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+}
+resp = requests.get(url, headers=headers)
+check_status(resp)
+# git_repos_names = []
+for i in resp.json():
+    n = i['name']
+    try:
+        git.Git().clone(i['git_url'])
+        print(n + " cloned successfully!")
+        # git_repos_names.append(n)
+        # print(git_repos_names)
+    except git.exc.GitCommandError:
+        print(n + " already cloned!")
+    finally:
+        run_scanner(n)
+
+# run_scanner(['java-codecov-example-master', "some_scripts"])
 # temp = git_clone.start()
 # print(temp)
 # run_scanner(git_clone.start())
